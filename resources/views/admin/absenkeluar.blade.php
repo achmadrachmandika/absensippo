6b476cd2383edd92c00f4e36529559606418a54f
satriiadaffa <satriiadaffa@gmail.com>
2024-02-26 16:09:21 +0700

    menambah-search-bar-admin

--- resources/views/admin/absenkeluar.blade.php
+++ resources/views/admin/absenkeluar.blade.php
@@ -2,6 +2,43 @@
 
 @section('content')
 
+<style>
+    * {
+      box-sizing: border-box;
+    }
+    
+    #myInput {
+      background-image: url('/css/searchicon.png');
+      background-position: 10px 10px;
+      background-repeat: no-repeat;
+      width: 100%;
+      font-size: 16px;
+      padding: 12px 20px 12px 40px;
+      border: 1px solid #ddd;
+      margin-bottom: 12px;
+    }
+    
+    #myTable {
+      border-collapse: collapse;
+      width: 100%;
+      border: 1px solid #ddd;
+      font-size: 18px;
+    }
+    
+    #myTable th, #myTable td {
+      text-align: left;
+      padding: 12px;
+    }
+    
+    #myTable tr {
+      border-bottom: 1px solid #ddd;
+    }
+    
+    #myTable tr.header, #myTable tr:hover {
+      background-color: #f1f1f1;
+    }
+</style>
+
 <div class="container">
     <div class="row">
         <div class="col-md-12">
@@ -17,8 +54,9 @@
                         <p>{{ $message }}</p>
                     </div>
                     @endif
+                    <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Cari.." title="Type in a name">
                     <div class="table-responsive">
-                        <table class="table table-striped">
+                        <table id="myTable" class="table table-striped">
                             <thead class="bg-secondary text-white">
                                 <tr>
                                     <th>#</th>
@@ -51,4 +89,33 @@
         </div>
     </div>
 </div>
+
+    
+    <script>
+    function myFunction() {
+      var input, filter, table, tr, td, i, txtValue;
+      input = document.getElementById("myInput");
+      filter = input.value.toUpperCase();
+      table = document.getElementById("myTable");
+      tr = table.getElementsByTagName("tr");
+      for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+          txtValue = td.textContent || td.innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            tr[i].style.display = "";
+          } else {
+            tr[i].style.display = "none";
+          }
+        }       
+      }
+    }
+    </script>
+
+
+
+
+
+
+
 @endsection
